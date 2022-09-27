84c45af55f8f704d3f7ad9c546aae5aa0149e10f
AsaadDadoush <asaaddaadouch246@gmail.com>
2022-09-27 23:33:45 +0300

    first commit

---
+++ sc2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## author: Asaad .W Daadouch
+## This program was made to check if there is a * symbol in a given path
+ 
+if  grep -q "[*]" <<< "$1"  		 ## check if there is a * symbol in the current path
+then					 ## do the action 
+	echo "Symbol is not required"	 ## print out ack there is no need for * if there is a * symbol  
+else				         ## check for other condition 
+	echo ' "*" is required'          ## print out a statment if * symbol not found 
+	echo ""$1"/*"                    ## add /* after the $1
+fi					 ## close the statment
